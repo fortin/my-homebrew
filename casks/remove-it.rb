e608da7cd2d6ed89e753a92ff4cd96cfa5f3d1c6
Antonio Fortin <antonio@kikodo.io>
2024-10-22 23:13:47 +0200

    Add remove-it cask

---
+++ casks/remove-it.rb
@@ -0,0 +1,11 @@
+cask "remove-it" do
+  version "1.4.2"
+  sha256 "8de36617ca7e69c62b2096662f20c785dfec4a2d92705b544a9a7c7faa7e6e60"
+
+  url "https://www.osxbytes.com/RemoveIt.dmg"
+  name "Remove-It"
+  desc "Quite Simply The Most Effective and Efficient Uninstaller for macOS"
+  homepage "https://osxbytes.com/remove-it.html"
+
+  app "Remove-It.app"
+end
