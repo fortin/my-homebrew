59533eb3b2e1fff46cbefdf8f5036806317296de
Antonio Fortin <antonio@kikodo.io>
2024-10-25 22:59:31 +0200

    Add minus.app cask

---
+++ casks/minus.rb
@@ -0,0 +1,11 @@
+cask "minus" do
+  version "1.0.99.143"
+  sha256 "2232fec5ec811e1c7c91158160e3849436360cd214bc15bce97cc28d5b11e230"
+
+  url "https://downloads.minus.io/mac/minus.dmg"
+  name "Minus"
+  desc "Actually Do Work"
+  homepage "https://www.minus.app/"
+
+  app "Minus.app"
+end
